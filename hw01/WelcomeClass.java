f4cd9573fb1318a0db8794d385efbd5af5e3d4fd
Juan Pablo Ferrer Socorro <jpf318@lehigh.edu>
2014-09-01 03:27:44 +0000

    WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,24 @@
+////////////////////////////////////////
+// Juan Pablo Ferrer
+// WelcomeClass HW#1
+//
+//  first compile the program
+//      javac WelcomeClass/java
+//  run the program
+//      java WelcomeClass//
+//
+public class WelcomeClass{
+    public static void main(String[] args) {
+        System.out.println("----------------------");
+        System.out.println("|      Welcome       |");
+        System.out.println("----------------------");
+        System.out.println("   ^  ^  ^  ^  ^  ^   ");
+    //In order to eliminate an escape character (\) you just
+    //need to put another escape character to cancel it out
+        System.out.println("  / \\/ \\/ \\/ \\/ \\/ \\  ");
+        System.out.println(" |-J--P--F--3--1--8-| ");
+        System.out.println("  \\ /\\ /\\ /\\ /\\ /\\ /  ");
+        System.out.println("   v  v  v  v  v  v   ");
+        System.out.println("I was born and raised in Venezuela. Although things may be different here, I still love Lehigh!");
+    }
+}
